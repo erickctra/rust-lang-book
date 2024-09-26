0e0a815370f697af016afda4e22ff98838c70550
Erick Cintra <erickcintra@proton.me>
2024-09-25 22:51:50 -0600

    Handling invalid input

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
     println!("Guess the number!");
-    println!("Please input your guess.");
+    println!("The secret number is: {}", secret_number);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess");
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        // .read_line() return a io::Result
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        // shadow the previous value of guess with a new one
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
+    // '&' Reference: the & notation is used
+    // to create references to variables.
+    // This allows you to access the value of a
+    // variable without transferring ownership
+    // of it. Let's take a closer look at this:
 }
